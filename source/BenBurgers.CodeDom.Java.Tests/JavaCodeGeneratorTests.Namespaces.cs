24019cbc572a43202b5d2ecd79b5a332c1f942a2
Ben Burgers <ben@benburgers.eu>
2025-06-07 17:40:10 +0200

    Java: Namespace

---
+++ source/BenBurgers.CodeDom.Java.Tests/JavaCodeGeneratorTests.Namespaces.cs
@@ -0,0 +1,20 @@
+﻿using System.CodeDom;
+
+namespace BenBurgers.CodeDom.Java.Tests;
+
+public sealed partial class JavaCodeGeneratorTests
+{
+    private sealed class Namespaces : TheoryData<CodeNamespace, string>
+    {
+        public Namespaces()
+        {
+            const string Namespace1Name = "Test";
+            var namespace1 = new CodeNamespace(Namespace1Name);
+            this.Add(namespace1,
+"""
+package Test;
+
+""");
+        }
+    }
+}
---
+++ source/BenBurgers.CodeDom.Java.Tests/JavaCodeGeneratorTests.cs
@@ -0,0 +1,23 @@
+﻿using BenBurgers.CodeDom.Java.Compiler;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Text;
+
+namespace BenBurgers.CodeDom.Java.Tests
+{
+    public sealed partial class JavaCodeGeneratorTests
+    {
+        [Theory(DisplayName = "Namespace tests")]
+        [ClassData(typeof(Namespaces))]
+        public void NamespaceTests(CodeNamespace ns, string expected)
+        {
+            var stringBuilder = new StringBuilder();
+            using var stringWriter = new StringWriter(stringBuilder);
+            var generator = new JavaCodeGenerator();
+            generator.GenerateCodeFromNamespace(ns, stringWriter, new CodeGeneratorOptions());
+            stringWriter.Flush();
+            var actual = stringBuilder.ToString();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
